6ea67d4dc8239980086c69a612ab3bdcb7050acf
Nanthana2003 <whipsmartnanthana2003@gmail.com>
2024-03-20 22:50:29 +0530

    second commit

--- app/src/main/java/com/example/myapplication/Activity_2.java
+++ app/src/main/java/com/example/myapplication/Activity_2.java
@@ -1,7 +1,14 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +16,69 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapplication.views.CameraPreview;
+
+import java.io.IOException;
+
+
 public class Activity_2 extends AppCompatActivity {
+    private Camera mCamera;
+    private CameraPreview mPreview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_2);
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.second), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+
+
+    public boolean checkCameraHardware(View view) {
+        Context context = getApplicationContext();
+        Log.w("cam", "entered");
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            Log.w("camera", "working");
+            return true;
+
+        } else {
+            // no camera on this device
+            Log.w("camera", "not detected");
+            return false;
+
+        }
+//
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+
+
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+            Log.w("app:","error in opening camera");
+        }
+        return c; // returns null if camera is unavailable
     }
 
 
 }
+
+/** A basic Camera preview class */
+
